a890e09caf744f098eefa204229c8cedb36afb72
Nicolas Romero Ortiz <romero_nikolas@hotmail.com>
2023-06-22 19:07:12 -0300

    Estructura de proyecto

---
+++ Grupo-10-OO2-2023/src/main/java/app/Grupo10Oo22023Application.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo10Oo22023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo10Oo22023Application.class, args);
+	}
+
+}
---
+++ Grupo-10-OO2-2023/src/test/java/app/Grupo10Oo22023ApplicationTests.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo10Oo22023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
